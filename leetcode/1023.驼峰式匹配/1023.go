d6ec67e36aade708b53805c75c049a194140f3af
Ohandsomeboy <577554980@qq.com>
2023-04-14 22:15:11 +0800

    2023.4.14

---
+++ leetcode/1023.驼峰式匹配/1023.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"unicode"
+)
+
+func camelMatch(queries []string, pattern string) []bool {
+	n := len(queries)
+	res := make([]bool, n)
+	for i := 0; i < n; i++ {
+		res[i] = true
+		p := 0
+		for _, c := range queries[i] {
+			if p < len(pattern) && pattern[p] == byte(c) {
+				p++
+			} else if unicode.IsUpper(c) { // 判断这个字符是小写还是大写，大写返回true，小写返回fasle
+				res[i] = false
+				break
+			}
+		}
+		if p < len(pattern) {
+			res[i] = false
+		}
+	}
+	return res
+}
+
+func main() {
+	var queries = []string{"FooBar", "FooBarTest"}
+	var s string = "FB"
+	fmt.Println(camelMatch(queries, s))
+}
