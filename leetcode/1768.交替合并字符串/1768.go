bb5b75f13d7e6993d38af2f5bdd53363878a1c4f
Ohandsomeboy <577554980@qq.com>
2023-04-19 10:02:01 +0800

    2023.4.19

--- leetcode/1768.交替合并字符串/1768.go
+++ leetcode/1768.交替合并字符串/1768.go
@@ -1,24 +1,39 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func mergeAlternately(word1 string, word2 string) string {
-	var s string
-	//n1 := len(word1)
-	//n2 := len(word2)
-	//var min int
-	//if n1 > n2 {
-	//	min = n2
-	//
-	//} else if n2 > n1 {
-	//	min = n1
-	//} else if n1 == n2 {
-	//	min = n1
-	//}
-	//for i := 0; i < min; i++ {
-	//	s = append(s, encodeToUtf8(word1[i]))
-	//}
-	return s
+	var s strings.Builder
+	n1 := len(word1)
+	n2 := len(word2)
+	var min int
+	if n1 > n2 {
+		min = n2
+
+	} else if n2 > n1 {
+		min = n1
+	} else if n1 == n2 {
+		min = n1
+	}
+	i := 0
+	for ; i < min; i++ {
+		s.WriteRune(rune(word1[i]))
+		s.WriteRune(rune(word2[i]))
+	}
+	if n1 > n2 {
+		s.WriteString(word1[i:])
+	} else {
+		s.WriteString(word2[i:])
+	}
+	str := s.String()
+	return str
 }
 
 func main() {
-
+	var s1 string = "abc"
+	var s2 string = "pqr"
+	fmt.Println(mergeAlternately(s1, s2))
 }
